a551d5bc79dcd0303822bc24531671dbd1c5732a
Ing. Eduardo E. Enríquez <rekineke@hotmail.com>
2023-09-13 22:19:02 -0500

    Creamos funcion de escuchar eventos y dentro de ella, llamamos una funciona que nos indica en que parte ha hecho clic el usuario y así obtener la información del curso que ha elegido para agregar al carrito de compras

--- js/app.js
+++ js/app.js
@@ -1,19 +1,34 @@
-//creación variables
-const carrito = document.querySelector("#carrito");
-const contenedorCarrito = document.querySelector("#lista-carrito tbody");
-const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
-const listaCursos = document.querySelector("#lista-cursos");
+//variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const listaCursos = document.querySelector('#lista-cursos');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
 cargarEventListeners();
-function cargarEventListeners() {
-  //Cuando agregas un curso presionando "Agregar al Carrito"
-  listaCursos.addEventListener("click", agregarCurso);
+function cargarEventListeners(){
+    //cuando agregas un curso presionando "Agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
 }
 
-//Funciones
-function agregarCurso(e) {
+//funciones
+function agregarCurso(e){
   e.preventDefault();
-  if(e.target.classList.contains("agregar-carrito")){
-    console.log("Agregando al carrito");
+  if (e.target.classList.contains('agregar-carrito')){
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
   }
+}
+
+//lee el contenido del HTML al que le dimos clic y extrae la información del curso
+function leerDatosCurso(curso){
+  console.log(curso);
+  //crear un objeto con el contenido del curso actual
+  const infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1
+  }
+  console.log(infoCurso);
 }
